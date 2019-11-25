679d250967f523bc0dec91559347750430448d88
赵宝文 <bowen799923347@gmail.com>
2019-11-25 22:07:51 +0800

    init

---
+++ src/test/java/com/bowen/hogwarts/HogwartsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bowen.hogwarts;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HogwartsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
