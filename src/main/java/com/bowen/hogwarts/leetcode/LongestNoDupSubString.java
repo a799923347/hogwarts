0f08c8d9115371dcb2c0d4a59641d4b667f15ae5
shachi <shachi@wacai.com>
2019-12-02 14:20:35 +0800

    feature(leetcode):无重复字符的最长子串，官方题解

--- src/main/java/com/bowen/hogwarts/leetcode/LongestNoDupSubString.java
+++ src/main/java/com/bowen/hogwarts/leetcode/LongestNoDupSubString.java
@@ -54,4 +54,26 @@ public class LongestNoDupSubString {
     }
     return result;
   }
+
+  /**
+   * 滑动窗口
+   * 执行用时 :12 ms, 在所有 java 提交中击败了68.97%的用户
+   * 内存消耗 :36.3 MB, 在所有 java 提交中击败了98.00%的用户
+   */
+  public int lengthOfLongestSubstring2(String s) {
+    int n = s.length();
+    Set<Character> set = new HashSet<>();
+    int ans = 0, i = 0, j = 0;
+    while (i < n && j < n) {
+      // try to extend the range [i, j]
+      if (!set.contains(s.charAt(j))){
+        set.add(s.charAt(j++));
+        ans = Math.max(ans, j - i);
+      }
+      else {
+        set.remove(s.charAt(i++));
+      }
+    }
+    return ans;
+  }
 }
