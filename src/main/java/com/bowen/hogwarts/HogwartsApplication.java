8c3137cc9646da85a31a527cf83f98c027c6babb
赵宝文 <bowen799923347@gmail.com>
2019-12-05 22:38:11 +0800

    fix(boot starter):启动时去掉data source依赖

--- src/main/java/com/bowen/hogwarts/HogwartsApplication.java
+++ src/main/java/com/bowen/hogwarts/HogwartsApplication.java
@@ -2,8 +2,9 @@ package com.bowen.hogwarts;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude= {DataSourceAutoConfiguration.class})
 public class HogwartsApplication {
 
 	public static void main(String[] args) {
