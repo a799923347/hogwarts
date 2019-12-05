da70966f4ded4075bf7ee132516fce16d4589678
shachi <shachi@wacai.com>
2019-12-05 15:05:41 +0800

    docs(leetcode):盛水最多的容器，首尾指针

--- src/main/java/com/bowen/hogwarts/leetcode/MaxArea.java
+++ src/main/java/com/bowen/hogwarts/leetcode/MaxArea.java
@@ -33,4 +33,30 @@ public class MaxArea {
     }
     return max;
   }
+
+  /**
+   * 首尾指针
+   *
+   * 最初我们考虑由最外围两条线段构成的区域。现在，为了使面积最大化，我们需要考虑更长的两条线段之间的区域。如果我们试图
+   * 将指向较长线段的指针向内侧移动，矩形区域的面积将受限于较短的线段而不会获得任何增加。但是，在同样的条件下，移动指向
+   * 较短线段的指针尽管造成了矩形宽度的减小，但却可能会有助于面积的增大。因为移动较短线段的指针会得到一条相对较长的线段，
+   * 这可以克服由宽度减小而引起的面积减小。
+   *
+   * 执行用时 :4 ms, 在所有 java 提交中击败了81.73%的用户
+   * 内存消耗 :38.7 MB, 在所有 java 提交中击败了94.37%的用户
+   */
+  public int maxArea2(int[] height) {
+    int max = 0;
+    int i = 0, j = height.length - 1;
+    for (; i < j; ) {
+      int currentArea = (j - i) * Math.min(height[i], height[j]);
+      max = Math.max(max, currentArea);
+      if (height[i] < height[j]) {
+        i++;
+      } else {
+        j--;
+      }
+    }
+    return max;
+  }
 }
--- src/test/java/com/bowen/hogwarts/leetcode/MaxAreaTest.java
+++ src/test/java/com/bowen/hogwarts/leetcode/MaxAreaTest.java
@@ -12,8 +12,16 @@ class MaxAreaTest {
   @Test
   void maxArea() {
     int[] height = {1,8,6,2,5,4,8,3,7};
-    int area = new MaxArea().maxArea(height);
+    int area = new MaxArea().maxArea2(height);
     System.out.println(area);
     assert 49 == area;
+  }
+
+  @Test
+  void maxArea2() {
+    int[] height = {6,4,3,1,4,6,99,62,1,2,6};
+    int area = new MaxArea().maxArea2(height);
+    System.out.println(area);
+    assert 62 == area;
   }
 }
