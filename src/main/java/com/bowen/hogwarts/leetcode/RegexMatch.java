be42b958aeaaa3a8e75c88ce13b861e6a99451e2
shachi <shachi@wacai.com>
2019-12-05 11:57:48 +0800

    feature(leetcode):正则表达式匹配

---
+++ src/main/java/com/bowen/hogwarts/leetcode/RegexMatch.java
@@ -0,0 +1,82 @@
+package com.bowen.hogwarts.leetcode;
+
+/**
+ * 给你一个字符串 s 和一个字符规律 p，请你来实现一个支持 '.' 和 '*' 的正则表达式匹配。
+ *
+ * '.' 匹配任意单个字符
+ * '*' 匹配零个或多个前面的那一个元素
+ * 所谓匹配，是要涵盖 整个 字符串 s的，而不是部分字符串。
+ *
+ * 说明:
+ *
+ * s 可能为空，且只包含从 a-z 的小写字母。
+ * p 可能为空，且只包含从 a-z 的小写字母，以及字符 . 和 *。
+ *
+ * 示例 1:
+ * 输入:
+ * s = "aa"
+ * p = "a"
+ * 输出: false
+ * 解释: "a" 无法匹配 "aa" 整个字符串。
+ *
+ * 示例 2:
+ * 输入:
+ * s = "aa"
+ * p = "a*"
+ * 输出: true
+ * 解释: 因为 '*' 代表可以匹配零个或多个前面的那一个元素, 在这里前面的元素就是 'a'。因此，字符串 "aa" 可被视为 'a' 重复了一次。
+ *
+ * 示例 3:
+ * 输入:
+ * s = "ab"
+ * p = ".*"
+ * 输出: true
+ * 解释: ".*" 表示可匹配零个或多个（'*'）任意字符（'.'）。
+ *
+ * 示例 4:
+ * 输入:
+ * s = "aab"
+ * p = "c*a*b"
+ * 输出: true
+ * 解释: 因为 '*' 表示零个或多个，这里 'c' 为 0 个, 'a' 被重复一次。因此可以匹配字符串 "aab"。
+ *
+ * 示例 5:
+ * 输入:
+ * s = "mississippi"
+ * p = "mis*is*p*."
+ * 输出: false
+ *
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/regular-expression-matching
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ *
+ * @author shachi
+ */
+public class RegexMatch {
+
+  /**
+   * 递归
+   * 如果没有星号（正则表达式中的 * ），问题会很简单——我们只需要从左到右检查匹配串 s 是否能匹配模式串 p 的每一个字符。
+   * 当模式串中有星号时，我们需要检查匹配串 s 中的不同后缀，以判断它们是否能匹配模式串剩余的部分。一个直观的解法就是用
+   * 回溯的方法来体现这种关系。
+   *
+   * 执行用时 :70 ms, 在所有 java 提交中击败了34.04%的用户
+   * 内存消耗 :41.1 MB, 在所有 java 提交中击败了64.92%的用户
+   */
+  public boolean isMatch(String text, String pattern) {
+    if (pattern.isEmpty()) {
+      return text.isEmpty();
+    }
+    boolean firstMatch = (!text.isEmpty() &&
+        (pattern.charAt(0) == text.charAt(0) || pattern.charAt(0) == '.'));
+
+    if (pattern.length() >= 2 && pattern.charAt(1) == '*') {
+      return (isMatch(text, pattern.substring(2)) ||
+          (firstMatch && isMatch(text.substring(1), pattern)));
+    } else {
+      return firstMatch && isMatch(text.substring(1), pattern.substring(1));
+    }
+
+  }
+
+}
