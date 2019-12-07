4008fd4d137a29d0e5141fd80ea530a5fcfc051e
赵宝文 <bowen799923347@gmail.com>
2019-12-07 23:43:42 +0800

    feature(三数之和):优化解法

--- src/main/java/com/bowen/hogwarts/leetcode/ThreeNumberSum.java
+++ src/main/java/com/bowen/hogwarts/leetcode/ThreeNumberSum.java
@@ -1,10 +1,14 @@
 package com.bowen.hogwarts.leetcode;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.HashSet;
 import java.util.List;
+import java.util.OptionalInt;
 import java.util.Set;
 import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 import java.util.stream.Stream;
 
 /**
@@ -45,6 +49,45 @@ public class ThreeNumberSum {
               resultCollection.add(integers);
             }
           }
+        }
+      }
+    }
+    return resultCollection;
+  }
+
+  /**
+   * 先对数组排序，从当前锚定的目标nums[i]开始，锚定位置右方使用首尾指针向中间扫描，对比暴力解法，时间复杂度为O(n^2)
+   * 执行用时 :53 ms, 在所有 java 提交中击败了46.62%的用户
+   * 内存消耗 :46.5 MB, 在所有 java 提交中击败了96.07%的用户
+   */
+  public List<List<Integer>> threeSum2(int[] nums) {
+    List<List<Integer>> resultCollection = new ArrayList<>();
+    Arrays.sort(nums);
+    for (int i = 0; i < nums.length - 2; i++) {
+      if (i > 0 && nums[i - 1] == nums[i]) {
+        continue;
+      }
+      if (nums[i] > 0) {
+        break;
+      }
+      int left = i + 1, right = nums.length - 1;
+      while (left < right) {
+        int sum = nums[i] + nums[left] + nums[right];
+        if (sum == 0) {
+          resultCollection
+              .add(Stream.of(nums[i], nums[left], nums[right]).collect(Collectors.toList()));
+          while (left < right && nums[left + 1] == nums[left]) {
+            left++;
+          }
+          while (left < right && nums[right - 1] == nums[right]) {
+            right--;
+          }
+          left++;
+          right--;
+        } else if (sum < 0) {
+          left++;
+        } else {
+          right--;
         }
       }
     }
