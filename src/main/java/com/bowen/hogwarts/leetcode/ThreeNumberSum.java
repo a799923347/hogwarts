27fbd30d27718418dea85974b7f25d2973ba364a
赵宝文 <bowen799923347@gmail.com>
2019-12-07 18:32:54 +0800

    feature(三数之和):暴力解法，超时

---
+++ src/main/java/com/bowen/hogwarts/leetcode/ThreeNumberSum.java
@@ -0,0 +1,53 @@
+package com.bowen.hogwarts.leetcode;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+/**
+ * 给定一个包含 n 个整数的数组 nums，判断 nums 中是否存在三个元素 a，b，c ，使得 a + b + c = 0 ？找出所有满足
+ * 条件且不重复的三元组。
+ *
+ * 注意：答案中不可以包含重复的三元组。
+ *
+ * 例如, 给定数组 nums = [-1, 0, 1, 2, -1, -4]，
+ *
+ * 满足要求的三元组集合为：
+ * [
+ * [-1, 0, 1],
+ * [-1, -1, 2]
+ * ]
+ *
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/3sum
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ *
+ * @author zhaobaowen
+ */
+public class ThreeNumberSum {
+
+  /**
+   * 暴力求解，时间复杂度O(n^3)，超时
+   */
+  public List<List<Integer>> threeSum(int[] nums) {
+    List<List<Integer>> resultCollection = new ArrayList<>();
+    Set<String> set = new HashSet<>();
+    for (int i = 0; i < nums.length - 2; i++) {
+      for (int j = i + 1; j < nums.length - 1; j++) {
+        for (int k = j + 1; k < nums.length; k++) {
+          if (nums[i] + nums[j] + nums[k] == 0) {
+            List<Integer> integers = Stream.of(nums[i], nums[j], nums[k]).sorted()
+                .collect(Collectors.toList());
+            if (set.add(integers.toString())) {
+              resultCollection.add(integers);
+            }
+          }
+        }
+      }
+    }
+    return resultCollection;
+  }
+}
