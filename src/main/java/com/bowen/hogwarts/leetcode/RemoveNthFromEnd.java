e767f9ade6873992fe75ae5d551e9186ed7df298
赵宝文 <bowen799923347@gmail.com>
2019-12-08 15:27:57 +0800

    feature(删除链表的倒数第N个节点):官方题解，双指针法

--- src/main/java/com/bowen/hogwarts/leetcode/RemoveNthFromEnd.java
+++ src/main/java/com/bowen/hogwarts/leetcode/RemoveNthFromEnd.java
@@ -51,4 +51,25 @@ public class RemoveNthFromEnd {
     return head;
   }
 
+  /**
+   * 官方题解，使用两个间隔为n的指针向后扫描，时间复杂度与removeNthFromEnd方法相同，但空间复杂度为O(1)
+   */
+  public ListNode removeNthFromEnd2(ListNode head, int n) {
+    ListNode dummy = new ListNode(0);
+    dummy.next = head;
+    ListNode first = dummy;
+    ListNode second = dummy;
+    // Advances first pointer so that the gap between first and second is n nodes apart
+    for (int i = 1; i <= n + 1; i++) {
+      first = first.next;
+    }
+    // Move first to the end, maintaining the gap
+    while (first != null) {
+      first = first.next;
+      second = second.next;
+    }
+    second.next = second.next.next;
+    return dummy.next;
+  }
+
 }
