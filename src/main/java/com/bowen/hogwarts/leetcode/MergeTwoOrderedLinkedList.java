9fd54c2ae26dc9e700cf7d4e3704f67870104b64
赵宝文 <bowen799923347@gmail.com>
2019-12-08 23:02:26 +0800

    feature(合并两个有序链表):递归解法

--- src/main/java/com/bowen/hogwarts/leetcode/MergeTwoOrderedLinkedList.java
+++ src/main/java/com/bowen/hogwarts/leetcode/MergeTwoOrderedLinkedList.java
@@ -38,4 +38,26 @@ public class MergeTwoOrderedLinkedList {
     return resultHead.next;
   }
 
+  /**
+   * 递归解法
+   * 时间复杂度：O(n + m)
+   * 空间复杂度：O(n + m)
+   */
+  public ListNode mergeTwoLists2(ListNode l1, ListNode l2) {
+    if (l1 == null) {
+      return l2;
+    }
+    else if (l2 == null) {
+      return l1;
+    }
+    else if (l1.val < l2.val) {
+      l1.next = mergeTwoLists(l1.next, l2);
+      return l1;
+    }
+    else {
+      l2.next = mergeTwoLists(l1, l2.next);
+      return l2;
+    }
+
+  }
 }
