bfd9241c4071ffafc9c25b1bc8c4585ac4ce387d
赵宝文 <bowen799923347@gmail.com>
2019-12-15 18:37:49 +0800

    feature(锁):同步代码块

--- src/main/java/com/bowen/hogwarts/lock/LockTest.java
+++ src/main/java/com/bowen/hogwarts/lock/LockTest.java
@@ -12,4 +12,11 @@ public class LockTest {
     System.out.println(myInt);
   }
 
+  public void test2() {
+    synchronized (this) {
+      myInt++;
+      System.out.println(myInt);
+    }
+  }
+
 }
