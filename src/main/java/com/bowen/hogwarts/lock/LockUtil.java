8b080af2fd7f65febb7ba7278ba1a8a2c931b97b
赵宝文 <bowen799923347@gmail.com>
2019-12-23 22:37:20 +0800

    feature(锁):对比静态方法与非静态方法被synchronized修饰时字节码的差异

--- src/main/java/com/bowen/hogwarts/lock/LockUtil.java
+++ src/main/java/com/bowen/hogwarts/lock/LockUtil.java
@@ -17,7 +17,15 @@ public class LockUtil {
     myInt++;
     log.info("计算器值={}", myInt);
     // 休眠一段时间，方式代码执行过快，保证有锁的竞争
-    // Thread.sleep(500);
+    Thread.sleep(100);
+  }
+
+  public synchronized void test3() throws InterruptedException {
+    log.info("当前获取锁的线程为={}", Thread.currentThread());
+    myInt++;
+    log.info("计算器值={}", myInt);
+    // 休眠一段时间，方式代码执行过快，保证有锁的竞争
+    Thread.sleep(100);
   }
 
   public void test2() {
