600a7801e629a7cf94ca22e2c60adb79876cfad2
赵宝文 <bowen799923347@gmail.com>
2019-12-25 02:55:01 +0800

    feature(两数相除):两数相除

--- src/test/java/com/bowen/hogwarts/leetcode/TwoNumberDivideTest.java
+++ src/test/java/com/bowen/hogwarts/leetcode/TwoNumberDivideTest.java
@@ -8,42 +8,63 @@ class TwoNumberDivideTest {
 
   @Test
   void divide() {
-    int divide = new TwoNumberDivide().divide(10, 3);
+    int divide = new TwoNumberDivide().divide2(10, 3);
     System.out.println(divide);
     assert divide == 3;
   }
 
   @Test
   void divide2() {
-    int divide = new TwoNumberDivide().divide(7, -3);
+    int divide = new TwoNumberDivide().divide2(7, -3);
     System.out.println(divide);
     assert divide == -2;
   }
 
   @Test
   void divide3() {
-    int divide = new TwoNumberDivide().divide(0, 1);
+    int divide = new TwoNumberDivide().divide2(0, 1);
     System.out.println(divide);
     assert divide == 0;
   }
 
   @Test
   void divide4() {
-    int divide = new TwoNumberDivide().divide(-1, 1);
+    int divide = new TwoNumberDivide().divide2(-1, 1);
     System.out.println(divide);
     assert divide == -1;
   }
 
   @Test
   void divide5() {
-    int divide = new TwoNumberDivide().divide(Integer.MAX_VALUE, 1);
+    int divide = new TwoNumberDivide().divide2(Integer.MAX_VALUE, 1);
     System.out.println(divide);
     assert divide == Integer.MAX_VALUE;
   }
 
   @Test
   void divide6() {
-    int divide = new TwoNumberDivide().divide(Integer.MIN_VALUE, 1);
+    int divide = new TwoNumberDivide().divide2(Integer.MIN_VALUE, 1);
+    System.out.println(divide);
+    assert divide == Integer.MIN_VALUE;
+  }
+
+  @Test
+  void divide7() {
+    int divide = new TwoNumberDivide().divide2(1, 1);
+    System.out.println(divide);
+    assert divide == 1;
+  }
+
+  @Test
+  void divide8() {
+    int divide = new TwoNumberDivide().divide2(Integer.MIN_VALUE, 2);
+    System.out.println(divide);
+    assert divide == -1073741824;
+  }
+
+  @Test
+  void divide9() {
+    int divide = new TwoNumberDivide().divide2(Integer.MIN_VALUE, -1);
     System.out.println(divide);
     assert divide == Integer.MAX_VALUE;
   }
