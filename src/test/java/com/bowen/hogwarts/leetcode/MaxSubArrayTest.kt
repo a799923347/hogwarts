d42aa57065ddab03edb87176b8cd713b77f2c6c9
赵宝文 <bowen799923347@gmail.com>
2019-12-28 23:28:20 +0800

    feature(最大子序和):暴力解法

---
+++ src/test/java/com/bowen/hogwarts/leetcode/MaxSubArrayTest.kt
@@ -0,0 +1,22 @@
+package com.bowen.hogwarts.leetcode
+
+import org.junit.jupiter.api.Test
+
+internal class MaxSubArrayTest {
+
+    @Test
+    fun maxSubArray() {
+        val nums = intArrayOf(-2, 1, -3, 4, -1, 2, 1, -5, 4)
+        val maxSum = MaxSubArray().maxSubArray(nums)
+        println(maxSum)
+        assert(maxSum == 6)
+    }
+
+    @Test
+    fun maxSubArray2() {
+        val nums = intArrayOf(-1)
+        val maxSum = MaxSubArray().maxSubArray(nums)
+        println(maxSum)
+        assert(maxSum == -1)
+    }
+}
