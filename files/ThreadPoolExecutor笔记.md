9f127cda8f64f84530c7d6ec3a31c24153f23d36
赵宝文 <bowen799923347@gmail.com>
2020-02-21 17:05:18 +0800

    test(mysql):初始化user表数据

--- files/ThreadPoolExecutor笔记.md
+++ files/ThreadPoolExecutor笔记.md
@@ -2,3 +2,5 @@
 private final AtomicInteger ctl = new AtomicInteger(ctlOf(RUNNING, 0));
 ```
 ctl:32位整型，高3位用于维护线程池的状态，低29位用于维护worker的数量(即线程的数量)。
+
+Worker继承了AQS，实现了Runnable接口，所以在exit的时候，使用AQS自有的Unsafe类+state状态的CAS来保证线程安全。Worker的消亡通过调用```Thread.interrupt()```方法。运行中的worker会循环从队列中获取任务，当从队列中获取不到任务<span style="border-bottom:2px dashed red;">或者</span>封装在worker中的task为null时worker则会执行exit逻辑。
--- src/test/java/com/bowen/hogwarts/dao/UserMapperTest.java
+++ src/test/java/com/bowen/hogwarts/dao/UserMapperTest.java
@@ -2,6 +2,7 @@ package com.bowen.hogwarts.dao;
 
 import com.bowen.hogwarts.HogwartsApplication;
 import com.bowen.hogwarts.dao.model.User;
+import java.util.Random;
 import javax.annotation.Resource;
 import org.junit.Ignore;
 import org.junit.Test;
@@ -25,5 +26,20 @@ public class UserMapperTest {
     User user = User.builder().name("wangwu").age(24).idNo("67453018375").gender("male").build();
     User insert = userMapperService.insert(user);
     assert insert.getId() != null;
+  }
+
+  @Ignore
+  @Test
+  public void initTableUser() throws InterruptedException {
+    Random random = new Random();
+    User user = User.builder().build();
+    for (int i = 1077; i < 100000; i++) {
+      user.setName("someone" + i);
+      user.setAge(24 + random.nextInt(30));
+      user.setIdNo(System.currentTimeMillis() + "" + random.nextInt(10));
+      user.setGender(random.nextInt(10) % 2 == 0 ? "male" : "female");
+      userMapperService.insert(user);
+      Thread.sleep(10);
+    }
   }
 }
