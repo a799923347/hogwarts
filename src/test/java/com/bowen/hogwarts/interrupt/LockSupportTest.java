1004deef3ddea33a56c62aaf7f400da9fbfd0921
赵宝文 <bowen799923347@gmail.com>
2020-03-10 13:29:55 +0800

    test(中断):interrupt和unpark都会唤醒被挂起的线程

--- src/test/java/com/bowen/hogwarts/interrupt/LockSupportTest.java
+++ src/test/java/com/bowen/hogwarts/interrupt/LockSupportTest.java
@@ -13,12 +13,12 @@ public class LockSupportTest {
     Thread myThread = new Thread(() -> {
       System.out.println("子线程 " + Thread.currentThread() + " 即将被挂起");
       LockSupport.park();
-      System.out.println("字线程 " + Thread.currentThread() + " 被恢复了");
+      System.out.println("子线程 " + Thread.currentThread() + " 被恢复了");
       System.out.println("子线程 " + Thread.currentThread() + " 中断状态: " + Thread.interrupted());
     });
     myThread.start();
     Thread.sleep(2000);
-    myThread.interrupt();
+    // myThread.interrupt();
     System.out.println("主线程 " + Thread.currentThread() + " 将要唤醒子线程");
     LockSupport.unpark(myThread);
     System.out.println("主线程 " + Thread.currentThread() + " 已触发子线程的唤醒");
